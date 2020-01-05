cc85c737c9dcb5f6394e37265354cb9fca8f3cfc
Zach Lang <zach.d.lang@gmail.com>
2020-01-05 23:40:00 +1300

    Adds axios defaults

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import axios from 'axios';
+
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.response.use(response => {
+  return response;
+}, error => {
+  if (error.response.status === 401) {
+    window.location = '/login';
+  } else {
+    return Promise.reject(error);
+  }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
