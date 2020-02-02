2f243e418f559e9e69ce0799fe7eb40718a39d65
Zach Lang <zach.d.lang@gmail.com>
2020-02-02 23:34:34 +1300

    Override Loading wheel styles using SASS

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,20 +3,9 @@ import { Loader as BulmaLoader } from 'react-bulma-components';
 
 class Loader extends Component {
   render() {
-    const styles = {
-      width: 100,
-      height: 100,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      marginTop: 200,
-      borderColor: 'turquoise',
-      borderTopColor: 'transparent',
-      borderRightColor: 'transparent'
-    };
-
     return(
       <BulmaLoader
-        style={styles}
+        className="loading-wheel"
       />
     )
   }
