0322c99c6b5d89819af725a7484fe231cb2f16c3
Zach Lang <zach.d.lang@gmail.com>
2020-03-03 00:29:48 +1300

    Work on basic layout of login page & colour scheme

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
-import { Hero, Container, Columns, Box, Button } from 'react-bulma-components';
-import { Form } from 'react-bulma-components';
+import { Hero, Container, Columns, Box, Button, Form, Icon } from 'react-bulma-components';
+import { User, Lock } from 'react-feather';
 import { getApiToken, login } from '../api/auth';
 
 class Login extends Component {
+  state = {
+    username: '',
+    password: '',
+  };
+
+  setUsername = (e) => {
+    this.setState({ username: e.target.value });
+  }
+
+  setPassword = (e) => {
+    this.setState({ password: e.target.value });
+  }
+
+  submitLogin = () => {
+    if (this.state.username && this.state.password) {
+      console.log('got creds!');
+    } else {
+      console.log('no creds :(');
+    }
+  }
+
   render() {
+    const { username, password } = this.state;
     return(
       <Hero size="fullheight" color="primary">
         <Hero.Body>
@@ -15,12 +37,24 @@ class Login extends Component {
               >
                 <Box>
                   <Form.Field>
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control>
-                      <Form.Input />
+                    <Form.Control iconLeft>
+                      <Form.Input onChange={this.setUsername} name="username" placeholder="Username" value={username} />
+                      <Icon align="left">
+                        <User />
+                      </Icon>
                     </Form.Control>
+                    <Form.Help color="danger">This field is required.</Form.Help>
                   </Form.Field>
-                  <Button>Hello World</Button>
+                  <Form.Field>
+                    <Form.Control iconLeft>
+                      <Form.Input onChange={this.setPassword} name="password" placeholder="Password" type="password" value={password} />
+                      <Icon align="left">
+                        <Lock />
+                      </Icon>
+                    </Form.Control>
+                    <Form.Help color="danger">This field is required.</Form.Help>
+                  </Form.Field>
+                  <Button fullwidth={true} color="success" onClick={this.submitLogin}>Login</Button>
                 </Box>
               </Columns.Column>
             </Columns>
