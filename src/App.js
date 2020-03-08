42c67affaaf6a3a1720a554db254c334606efa05
Zach Lang <zach.d.lang@gmail.com>
2020-03-08 15:23:58 +1300

    Fixes 404 component always showing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Menu from './components/Menu';
 import Home from './components/Home';
 import Login from './components/Login';
@@ -16,24 +16,26 @@ class App extends Component {
       <Router>
         <div className="App">
           <Menu />
-          <Route
-            exact
-            path='/'
-            component={Home}
-          />
-          <Route
-            path='/login'
-            component={Login}
-          />
-          <Route
-            path='/logout'
-            component={Logout}
-          />
-          <Route
-            path='/episodes'
-            component={EpisodeDateList}
-          />
-          <Route component={PageNotFound} />
+          <Switch>
+            <Route
+              exact
+              path='/'
+              component={Home}
+            />
+            <Route
+              path='/login'
+              component={Login}
+            />
+            <Route
+              path='/logout'
+              component={Logout}
+            />
+            <Route
+              path='/episodes'
+              component={EpisodeDateList}
+            />
+            <Route component={PageNotFound} />
+          </Switch>
         </div>
       </Router>
     );
